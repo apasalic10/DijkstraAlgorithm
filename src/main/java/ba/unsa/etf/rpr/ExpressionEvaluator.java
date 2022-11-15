97cfd738c4111f1e0bd654fe9288197512573ecd
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2022-11-15 21:22:54 +0100

    implemented evaluate method

--- src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
+++ src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
@@ -19,8 +19,52 @@ public class ExpressionEvaluator {
         String[] splits = str.split(" ");
 
         for(String x : splits){
+            if(x.equals("(")) {}
+            else if(x.equals(")")){
 
+                String operacija = operators.pop();
+                if(isBinaryOperator(operacija)){
+
+                    double a =operands.pop();
+                    double b = operands.pop();
+
+                    if(operacija.equals("+")){
+                        double rez = a + b;
+                        operands.push(rez);
+                    }
+                    else if(operacija.equals("-")){
+                        double rez = a - b;
+                        operands.push(rez);
+                    }
+                    else if(operacija.equals("*")){
+                        double rez = a * b;
+                        operands.push(rez);
+                    }
+                    else if(operacija.equals("/")){
+                        double rez = a / b;
+                        operands.push(rez);
+                    }
+
+                }
+                else{
+                    double a =operands.pop();
+                    double rez = Math.sqrt(a);
+                    operands.push(rez);
+                }
+            }
+            else if(isBinaryOperator(x) || isUnaryOperator(x)){
+                operators.push(x);
+            }
+            else{
+                try{
+                    operands.push(Double.parseDouble(x));
+                }
+                catch (NumberFormatException e){
+                    throw new RuntimeException("Unos nije validan!");
+                }
+            }
         }
 
+        return operands.pop();
     }
 }
