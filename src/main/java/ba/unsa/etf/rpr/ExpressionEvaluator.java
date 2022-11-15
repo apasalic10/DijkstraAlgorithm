91d70a6e55e01a653d6036d86419e4757a09480a
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2022-11-15 21:18:39 +0100

    added isBinaryOperator, isUnaryOperator and evaluate methods

--- src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
+++ src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
@@ -7,5 +7,20 @@ public class ExpressionEvaluator {
     Stack<String> operators = new Stack<String>();
     Stack<Double> operands = new Stack<Double>();
 
+    private boolean isBinaryOperator(String str){
+        return str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/");
+    }
 
+    private boolean isUnaryOperator(String str){
+        return str.equals("sqrt");
+    }
+
+    public Double evaluate(String str){
+        String[] splits = str.split(" ");
+
+        for(String x : splits){
+
+        }
+
+    }
 }
