40e5e4ed90b7877d50fc29aa8617ab9638c95105
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2022-11-15 21:11:56 +0100

    added attributes to ExpressionEvaluator class

--- src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
+++ src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
@@ -1,4 +1,11 @@
 package ba.unsa.etf.rpr;
 
+import java.util.Stack;
+
 public class ExpressionEvaluator {
+
+    Stack<String> operators = new Stack<String>();
+    Stack<Double> operands = new Stack<Double>();
+
+
 }
