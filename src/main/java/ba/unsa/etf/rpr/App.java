4555212ee326df7ae3bb587a014a0e35ea4775d6
apasalic10 <115503793+apasalic10@users.noreply.github.com>
2022-11-16 21:21:10 +0100

    fixed main method

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -10,7 +10,7 @@ public class App
                 System.out.println("Unijeli ste prazan string!");
             }
             else{
-                System.out.println(expEval.evaluate(args[0]));
+                System.out.println(Math.round(expEval.evaluate(args[0]) * 100. )/100.);
             }
         }
         catch (RuntimeException e ){
